5afd533d38d6ffcf1b4d9aa8cc259ae5ef747828
Chase Southard <chase.southard@gmail.com>
2016-05-15 22:16:19 -0400

    glued together

--- src/components/switch-box.js
+++ src/components/switch-box.js
@@ -5,25 +5,31 @@ export default class SwitchBox extends React.Component {
     super();
 
     this.state = {
-      isComplete: false
+      isComplete: false,
+      hasClicked: false
     };
+
+    this.startTime = null;
+    this.stopTime = null;
   }
 
   componentWillMount() {
-    setTimeout(() => {this._finish()}, 10000)
+    // setTimeout(() => {this._finish()}, 10000)
   }
 
   render() {
-    let color;
+    let boxDiv;
 
     if (this.state.isComplete) {
-      color = 'redBox';
+      boxDiv = <div className='redBox' onClick={this._record_click.bind(this)}></div>;
     } else {
-      color = 'greenBox';
+      boxDiv = <div className='greenBox'></div>;
     }
 
     return(
-      <div className={color}></div>
+      <div>
+        {boxDiv}
+      </div>
     );
   }
 
@@ -31,5 +37,34 @@ export default class SwitchBox extends React.Component {
     this.setState({
       isComplete: true
     });
+    this._start_timer();
+  }
+
+  _start_timer() {
+    this.startTime = performance.now();
+    console.log(this.startTime);
+  }
+
+  _stop_timer() {
+    this.stopTime = performance.now();
+    console.log(this.stopTime);
+  }
+
+  _reset() {
+  }
+
+  _timeout() {
+  }
+
+  _latency() {
+    return this.stopTime - this.startTime
+  }
+
+  _record_click() {
+    this.setState({
+      hasClicked: true
+    });
+    // todo: return if hasClicked is already true
+    this._stop_timer();
   }
 }
