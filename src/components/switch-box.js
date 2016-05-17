8f36f9c462cc1bf93cfb91bb5ee0cddb096ae788
Chase Southard <chase.southard@gmail.com>
2016-05-16 23:34:25 -0400

    hook up buttons; add results

--- src/components/switch-box.js
+++ src/components/switch-box.js
@@ -18,17 +18,30 @@ export default class SwitchBox extends React.Component {
   }
 
   render() {
-    let boxDiv;
+    let boxDiv = <div></div>
+    let resultsDiv = <div></div>
+    let actionButton = <div></div>
 
     if (this.state.isComplete) {
-      boxDiv = <div className='redBox' onClick={this._record_click.bind(this)}></div>;
+      if  (this.state.hasClicked) {
+        boxDiv = <div className='redBox'><p className='checked'>&#x2713;</p></div>;
+        resultsDiv = <div className='results'><p>{this._latency()} ms</p></div>
+        actionButton = <div><p><button onClick={this._reset.bind(this)}>TRY AGAIN!</button></p></div>
+      }
+      else {
+        // wonder if should detect key press instead of click
+        boxDiv = <div className='redBox' onClick={this._record_click.bind(this)}></div>;
+      }
     } else {
       boxDiv = <div className='greenBox'></div>;
+      actionButton = <div><p><button onClick={this._finish.bind(this)}>GO!</button></p></div>
     }
 
     return(
       <div>
         {boxDiv}
+        {resultsDiv}
+        {actionButton}
       </div>
     );
   }
@@ -42,29 +55,41 @@ export default class SwitchBox extends React.Component {
 
   _start_timer() {
     this.startTime = performance.now();
-    console.log(this.startTime);
+    console.log('startTime:', this.startTime);
   }
 
   _stop_timer() {
     this.stopTime = performance.now();
-    console.log(this.stopTime);
+    console.log('stopTime', this.stopTime);
   }
 
   _reset() {
+    this.startTime = null;
+    this.stopTime = null;
+    this.setState({
+      hasClicked: false
+    });
+    this.setState({
+      isComplete: false
+    });
   }
 
   _timeout() {
+    // maybe have some random timeout between green and red states
+  }
+
+  _countdown() {
+    // maybe have a countdown before the test starts
   }
 
   _latency() {
-    return this.stopTime - this.startTime
+    return (this.stopTime - this.startTime).toFixed(2)
   }
 
   _record_click() {
+    this._stop_timer();
     this.setState({
       hasClicked: true
     });
-    // todo: return if hasClicked is already true
-    this._stop_timer();
   }
 }
